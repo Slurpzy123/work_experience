42ddc222edaf41422fc607e3ce7b420c8abd4485
Calvin WorkExperience <calvin@theidol.com>
2024-03-07 12:24:32 +0000

    About me Page DONE

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // Imports
 const express = require('express')
 const app = express()
-const port = 3000
-
+const port = 3003
 
 // Static Files
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/css', express.static(__dirname + '/public/css'))
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/img', express.static(__dirname + '/public/img'))
 
 // Set Views
 app.set('views', './views')
@@ -21,7 +20,8 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { text: 'About Page'})
 })
+app.get('/counter', (req, res) => {
+    res.render('counter', { text: 'Counter'})
+})
 //  Listen on port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`))
-
-
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,113 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+// set initial number
+let count = 0;
+
+//select value
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+function increment() {
+  count++;
+  value.textContent = count;
+  colorchange();
+}
+function decrement() {
+  count--;
+  value.textContent = count;
+  colorchange();
+}
+function reset() {
+  count = 0;
+  value.textContent = count;
+  colorchange();
+}
+function colorchange() {
+  if (count > 0) {
+    value.style.color = "green";
   }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  if (count < 0) {
+    value.style.color = "red";
   }
+  if (count === 0) {
+    value.style.color = "#222";
+  }
+  if (count > 1000) {
+    value.style.color = gold;
+  }
+}
+
+
+
+
+
+
+// OLD CODE // OLD CODE // OLD CODE // OLD CODE // OLD CODE //
+// btns.forEac(function (btn) {h
+//     btn.addEventListener('click',function(e) {
+//     const styles = e.currentTarget.classList;
+//     console.log(styles);
+//     if(styles.contains('decrease')){
+//         count--;
+//     }
+//     else if(styles.contains('increase')){
+//         count++;
+//     }
+//     else{
+//         count = 0
+//     }
+//     if(count > 0){
+//         value.style.color = 'green'
+//     }
+//     if(count < 0){
+//         value.style.color = 'red'
+//     }
+//     if(count === 0){
+//         value.style.color = '#222'
+//     }
+//     if(count > 1000){
+//         value.style.color = gold
+//     }
+
+//      value.textContent = count;
+//     });
+
+// });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
